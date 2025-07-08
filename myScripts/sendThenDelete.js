0c8709b2c5fd25c1c9d0ae34df58b3b8e1b332c7
DeadLyBro <31214077+DeadLyBro@users.noreply.github.com>
2025-07-09 02:06:40 +0300

    Update sendThenDelete.js

--- myScripts/sendThenDelete.js
+++ myScripts/sendThenDelete.js
@@ -3,27 +3,27 @@ const { api } = require('../index.js')
 
 // Copy paste the below code inside the Browser Console
 ;(async () => {
-  const userInfo = await api.getCurrentUser()
+  const userInfo = (await api.getCurrentUser()).user;
 
-  api.update_teamId_and_channelId_withCurrentlyVisible()
-  let channelId = api.getConfig().channelId
+  api.update_teamId_and_channelId_withCurrentlyVisible();
+  let channelId = api.getConfig().channelId;
 
   // Send a message
-  let sentMessage = await api.sendMessage(channelId, `Hello! 👋 My name is ${userInfo.user.username}!`)
+  let sentMessage = (await api.sendMessage(channelId, `Hello! 👋 My name is ${userInfo.username}!`)).message;
 
-  await api.delay(2000)
+  await api.delay(2000);
 
   // Edit a message
-  let editedMessage = await api.editMessage(channelId, sentMessage.message.id, 'Hello, edited! ✌️')
+  let editedMessage = (await api.editMessage(channelId, sentMessage.id, 'Hello, edited! ✌️')).message;
 
-  await api.delay(2000)
+  await api.delay(2000);
 
   // Delete a message
-  await api.deleteMessage(channelId, editedMessage.message.id)
+  await api.deleteMessage(channelId, editedMessage.id);
 
-  await api.delay(2000)
+  await api.delay(2000);
 
   // Log the last 100 messages in the console
-  let messages = await api.getMessages(channelId)
-  console.log(messages)
+  let messages = (await api.getMessages(channelId)).messages;
+  console.log(messages);
 })()
