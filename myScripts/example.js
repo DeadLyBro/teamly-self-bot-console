9b24076bc98ab23ce60c14c531e5b61615493b4a
DeadLyBro <31214077+DeadLyBro@users.noreply.github.com>
2025-07-09 02:05:11 +0300

    Update example.js

--- myScripts/example.js
+++ myScripts/example.js
@@ -3,7 +3,7 @@ const { api } = require('../index.js')
 
 // Copy paste the below code inside the Browser Console
 ;(async () => {
-  const userInfo = await api.getCurrentUser()
+  const userInfo = (await api.getCurrentUser()).user;
 
-  console.log(userInfo.user.username)
+  console.log(userInfo.username);
 })()
