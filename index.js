5adec2b7c315f9b0c4c13f4bfaeee46e314509f8
DeadLyBro <31214077+DeadLyBro@users.noreply.github.com>
2025-07-14 15:11:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@
         Accept: '*/*',
         'Accept-Language': 'tr',
         Cookie: cookie,
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Teamly/0.0.21-beta Chrome/130.0.6723.137 Electron/33.2.1 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Teamly/0.0.31-beta Chrome/130.0.6723.137 Electron/33.2.1 Safari/537.36',
       },
       ...options,
     }
